1d48ded79a2845020229a17ac685ba366ce795db
rash-ad <Rashad.mohd2004@gmail.com>
2026-05-21 23:01:18 +0530

    Add initial module demonstrations with math, random, datetime, and os

---
+++ basics/modules/modules.py
@@ -0,0 +1,9 @@
+print("welcome to Modules")
+import math
+print("The square root of 16 is:", math.sqrt(16))
+import random
+print("A random number between 1 and 10 is:", random.randint(1,10))
+import datetime
+print("Today's date is:", datetime.date.today())
+import os
+print("Current working directory:", os.getcwd())
